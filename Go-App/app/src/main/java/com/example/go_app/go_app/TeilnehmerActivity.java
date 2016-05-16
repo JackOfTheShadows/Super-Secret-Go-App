1ef56ee59f46a517d4d0d6b6719d84a4bc2181bb
JackOfTheShadows <uzcyd@student.kit.edu>
2016-05-16 12:59:22 +0200

    no idea

--- Go-App/app/src/main/java/com/example/go_app/go_app/TeilnehmerActivity.java
+++ Go-App/app/src/main/java/com/example/go_app/go_app/TeilnehmerActivity.java
@@ -5,63 +5,59 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
+public class TeilnehmerActivity extends AppCompatActivity implements View.OnClickListener, PopupMenu.OnMenuItemClickListener {
 
-    //something
     ImageButton menu_button;
-    ImageButton teilnehmer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        menu_button = (ImageButton) findViewById(R.id.menu_termine);
+        setContentView(R.layout.activity_teilnehmer);
+        menu_button = (ImageButton) findViewById(R.id.menu_teilnehmer);
         menu_button.setOnClickListener(this);
-        teilnehmer = (ImageButton) findViewById(R.id.teilnehmer);
-        teilnehmer.setOnClickListener(this);
     }
 
     public static void start(Activity activity) {
-        Intent intent = new Intent(activity, MainActivity.class);
+        Intent intent = new Intent(activity, TeilnehmerActivity.class);
         activity.startActivity(intent);
     }
 
     public void showPopUp(View v) {
         PopupMenu popup = new PopupMenu(this, v);
-        popup.setOnMenuItemClickListener(MainActivity.this);
+        popup.setOnMenuItemClickListener(TeilnehmerActivity.this);
         MenuInflater inflater = popup.getMenuInflater();
-        inflater.inflate(R.menu.menu_main, popup.getMenu());
+        inflater.inflate(R.menu.menu_teilnehmer, popup.getMenu());
         popup.show();
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.menu_termine) {
+        if (v.getId() == R.id.menu_teilnehmer) {
             showPopUp(v);
-        }else if (v.getId() == R.id.teilnehmer) {
-            TeilnehmerActivity.start(this);
         }
     }
 
     @Override
     public boolean onMenuItemClick(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.neuer_termin_main:
+            case R.id.home_teilnehmer:
+                TeilnehmerActivity.start(this);
+                return true;
+            case R.id.neuer_termin_teilnehmer:
                 TerminActivity.start(this);
                 return true;
-            case R.id.groups_main:
+            case R.id.groups_teilnehmer:
                 GroupsActivity.start(this);
                 return true;
-            case R.id.settings_main:
+            case R.id.settings_teilnehmer:
                 SettingsActivity.start(this);
                 return true;
-            case R.id.about_main:
+            case R.id.about_teilnehmer:
                 AboutActivity.start(this);
                 return true;
             default:
